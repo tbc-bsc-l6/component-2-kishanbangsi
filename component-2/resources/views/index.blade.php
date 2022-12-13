1b9781b0b7d2bbda90aeb1d5d61e8de3046649eb
Kishan <bang.siki2009@gmail.com>
2022-12-13 20:58:07 +0545

    View all posts

--- component-2/resources/views/index.blade.php
+++ component-2/resources/views/index.blade.php
@@ -4,15 +4,15 @@
     <h2 class="fw-bold text-center mt-5 mb-5">All Blogs</h2>
 
     <div class="container">
-        <div class="card">
+        @foreach ($posts as $post)
+        <div class="col-md-10 card mx-auto mt-5 px-5">
             <div class="card-body">
-                <h5 class="card-title fs-3">First Blog</h5>
-                <p class="card-text mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                    Pariatur iste ab neque dolorum earum facere placeat vel, doloremque
-                    provident consequuntur.
-                </p>
+                <h5 class="card-title fs-3">{{ $post->title }}</h5>
+                <p class="text-muted">{{ date('d/m/Y', strtotime($post->updated_at)); }}</p>
+                <p class="card-text mt-4">{{ $post->description }}</p>
                 <a href="#" class="btn btn-primary">Read More</a>
             </div>
         </div>
+        @endforeach
     </div>
 @endsection
