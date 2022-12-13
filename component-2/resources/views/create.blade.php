1a0ba48b9ca21a390cdf930350882271161229bb
Kishan <bang.siki2009@gmail.com>
2022-12-13 14:20:40 +0545

    Create new blade files

---
+++ component-2/resources/views/create.blade.php
@@ -0,0 +1,22 @@
+@extends('layout')
+
+@section("body")
+    <h2 class="fw-bold text-center mt-5 mb-5">Create New Blog</h2>
+
+    <div class="container col-md-8">
+        {{-- Form to create new blog --}}
+        <form action="" method="POST">
+            @csrf
+            <div class="mt-3">
+                <label for="title" class="form-label">Title</label>
+                <input type="text" name="title" id="title" min="3" class="form-control" required>
+            </div>
+            <div class="mt-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea name="description" id="description" cols="30" rows="10" class="form-control"></textarea>
+            </div>
+            <a href="/" class="btn btn-secondary mt-3">Cancel</a>
+            <button type="submit" class="btn btn-outline-primary mt-3">Create Post</button>
+        </form>
+    </div>
+@endsection
---
+++ component-2/resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layout')
+
+@section('body')
+    <h2 class="fw-bold text-center mt-5 mb-5">All Blogs</h2>
+
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title fs-3">First Blog</h5>
+                <p class="card-text mt-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                    Pariatur iste ab neque dolorum earum facere placeat vel, doloremque
+                    provident consequuntur. Voluptatum, vero nostrum a quibusdam dolore aperiam?
+                    Nulla, atque aliquam.
+                </p>
+                <a href="#" class="btn btn-primary">Read More</a>
+            </div>
+        </div>
+    </div>
+@endsection
