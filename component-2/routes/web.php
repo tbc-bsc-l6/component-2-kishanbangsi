55d65f5fa53e882c5da5e333a17f0f2783d2018d
Kishan <bang.siki2009@gmail.com>
2022-12-13 14:19:43 +0545

    Add new routes

--- component-2/routes/web.php
+++ component-2/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route for homepage
+Route::get('/', [BlogController::class, "index"]);
+
+// Route to create a blog
+Route::get('/create', [BlogController::class, "create"]);
