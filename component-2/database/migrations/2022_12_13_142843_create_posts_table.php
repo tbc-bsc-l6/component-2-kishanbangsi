7d12051f22f469152fc21178d5abeb97b6bab0fe
Kishan <bang.siki2009@gmail.com>
2022-12-14 18:27:09 +0545

    Add new column slug to table

--- component-2/database/migrations/2022_12_13_142843_create_posts_table.php
+++ component-2/database/migrations/2022_12_13_142843_create_posts_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string("title");
             $table->text("description");
+            $table->text("slug");
             $table->timestamps();
         });
     }
