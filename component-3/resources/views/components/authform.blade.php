4351e3819d29c72fb83cbf1648b6b3a082d0fefc
Kishan <bang.siki2009@gmail.com>
2022-12-23 17:29:54 +0545

    Create component for login & register

---
+++ component-3/resources/views/components/authform.blade.php
@@ -0,0 +1,8 @@
+<div class="my-5">
+    <label for="email" class="block text-white my-1">Email</label>
+    <input type="email" name="email" id="email" placeholder="Email" class="text-white py-2 px-4 w-full bg-indigo-500 outline-none rounded-md">
+</div>
+<div class="my-5">
+    <label for="password" class="block text-white my-1">Password</label>
+    <input type="password" name="password" id="password" placeholder="Password" class="text-white py-2 px-4 w-full bg-indigo-500 outline-none rounded-md">
+</div>
