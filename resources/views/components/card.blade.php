e3ce558aef89454c4bc2f4fdfb79dd004598433d
Kishan <bang.siki2009@gmail.com>
2022-12-27 17:41:39 +0545

    Created a product card component

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,10 @@
+<div class="col-span-1 flex flex-col py-4 px-6 items-center border border-gray-200 rounded-lg shadow-lg">
+    <img src="images/bookcover1.jpg" alt="bookcover" class="my-5 w-36 h-56">
+    <div class="text-center mx-2 px-1 border-t border-gray-300 lg:text-start">
+        <h3 class="font-semibold text-xl text-center mt-5">Crime and Punishment</h3>
+        <p class="mt-3 mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        <p class="font-semibold">Author: <span class="font-normal ml-2">Fyodor Dostoevsky</span></p>
+        <p class="font-semibold my-1">Price: <span class="font-normal ml-2">$20</span></p>
+        <a href="#" class="font-semibold hover:text-violet-500">View More ></a>
+    </div>
+</div>
