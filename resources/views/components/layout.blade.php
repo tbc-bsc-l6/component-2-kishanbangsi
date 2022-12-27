821963c6c6b8f50570d69d699e48b25257c43f09
Kishan <bang.siki2009@gmail.com>
2022-12-27 17:39:34 +0545

    Added navbar and footer as components

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,13 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     @vite('resources/css/app.css')
-    <title>Products</title>
+    <title>SmallMart</title>
 </head>
-<body class="bg-blue-500">
-    <nav class="flex justify-end space-x-6 py-2 px-10 border-b border-blue-600">
-        <a href="{{ route('index') }}" class="text-white bg-transparent border border-blue-800 outline-none py-2 px-5 rounded-md transition ease-in-out duration-500 hover:bg-blue-800">Home</a>    
-        <a href="{{ route('create') }}" class="text-white bg-transparent border border-blue-800 outline-none py-2 px-5 rounded-md transition ease-in-out duration-500 hover:bg-blue-800">Create</a>
-    </nav>
+<body class="poppins">
+    <x-nav />
     {{ $slot }}
+    <x-footer />
+    <script src="js/script.js"></script>
 </body>
 </html>
